229cb433328663c8887899b26c31fe69d5e9000e
“rami699” <omarhegazyking@gmail.com>
2025-08-24 09:27:50 -0400

    finished task 4

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,8 @@ function ProductList({ onHomeClick }) {
     const [addedToCart, setAddedToCart] = useState({});
     const cart = useSelector(state => state.cart.items);
     const dispatch = useDispatch();
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -283,18 +285,16 @@ function ProductList({ onHomeClick }) {
                     <div>
   <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
     <h1 className='cart'>
+    <span style={{
+          }}>
+            {totalQuantity}
+          </span>   
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
         <rect width="156" height="156" fill="none"></rect>
         <circle cx="80" cy="216" r="12"></circle>
         <circle cx="184" cy="216" r="12"></circle>
         <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
-      </svg>
-      {/* Simple number next to cart */}
-      {cart.length > 0 && (
-        <span style={{ marginLeft: '8px', fontWeight: 'bold', color: 'white' }}>
-          {cart.reduce((sum, item) => sum + item.quantity, 0)}
-        </span>
-      )}
+      </svg>       
     </h1>
   </a>
 </div>
